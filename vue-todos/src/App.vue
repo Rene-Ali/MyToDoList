591363d7dc8fad09478981231110c9e2700f06b3
René-Ali Günürün <renee.guenueruen@gmail.com>
2023-07-19 22:55:12 +0200

    iconify and dependencies added

--- vue-todos/src/App.vue
+++ vue-todos/src/App.vue
@@ -1,18 +1,34 @@
 <script setup>
 import {RouterView} from "vue";
+import ToDoHeader from "@/components/ToDoHeader.vue";
 
 
 </script>
 
 <template>
+  <ToDoHeader/>
   <header>
-   <RouterView/>
+    <RouterView/>
+    //Stand 20:09 tbc
 
   </header>
 
 
 </template>
 
-<style scoped>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,wght@1,100;1,400&family=Ubuntu+Mono&family=Ubuntu:ital@1&display=swap');
+
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
 
 </style>
---
+++ vue-todos/src/components/ToDoHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <img src="../assets/Vue_Logo_Black.png" alt="">
+        <h1> Vue Todos</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink to="/">Home</RouterLink>
+        <Routerlink to="/about">About</Routerlink>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import {Routerlink} from "vue-router";
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
